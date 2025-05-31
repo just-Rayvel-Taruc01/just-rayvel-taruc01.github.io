9cea54b3ce71e0f798d2a4a75ab03a7ee65454e5
Rayvel Taruc <rayvelm.taruc02@gmail.com>
2025-05-31 20:04:18 +0800

    Update deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,11 @@
-const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages';
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'just-rayvel-taruc01.github.io'; // ✅ your repo name
-const baseUrl = isGhPages && isProd ? `/${repo}` : '';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  crossOrigin: 'anonymous',
+  output: 'export',
   trailingSlash: true,
-  env: {
-    baseUrl, // ✅ now this works
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
+  reactStrictMode: true,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-      },
-    ],
   },
-  output: 'export',
-  basePath: baseUrl,
-  assetPrefix: baseUrl + '/',
 };
 
-module.exports = nextConfig; // ✅ CommonJS for compatibility with Next.js
+module.exports = nextConfig;
