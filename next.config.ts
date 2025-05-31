e65950d3e81bf0dcbdc907f6a63291c217aa473f
Rayvel Taruc <rayvelm.taruc02@gmail.com>
2025-06-01 06:53:00 +0800

    Update config

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Important for GH Pages static export to work with relative paths
-  assetPrefix: '/',
-  basePath: '',
 };
 
 module.exports = nextConfig;
