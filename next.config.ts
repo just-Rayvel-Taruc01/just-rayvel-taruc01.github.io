fbe8372962ee62f6c3083929dd95e239d32aef1e
Rayvel Taruc <rayvelm.taruc02@gmail.com>
2025-05-31 19:05:56 +0800

    Fixing CSS 1

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/just-rayvel-taruc01.github.io',
-  assetPrefix: '/just-rayvel-taruc01.github.io/',
+  assetPrefix: '/just-rayvel-taruc01.github.io',
+  trailingSlash: true,
   output: 'export',
   reactStrictMode: true,
   images: {
