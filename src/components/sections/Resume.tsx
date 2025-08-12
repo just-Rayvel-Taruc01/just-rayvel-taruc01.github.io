9a4ac61704142005ae23c261d5630ad2fb5db303
Rayvel Taruc <rayvelm.taruc02@gmail.com>
2025-08-12 23:39:01 +0800

    <feat> Resume: Adding Experience and Internship part

--- src/components/sections/Resume.tsx
+++ src/components/sections/Resume.tsx
@@ -1,4 +1,4 @@
-import { FaBook } from 'react-icons/fa';
+import { FaBook, FaBriefcase } from 'react-icons/fa';
 import Image from 'next/image';
 import Skills from '../ui/Skills';
 
@@ -6,6 +6,35 @@ export function Resume() {
   return (
     <div className="bg-[var(--card-bg)] p-6 rounded-lg shadow-md">
       <h2 className="text-3xl font-bold mb-4 text-[var(--headline)]">Resume</h2>
+
+      {/* xperience */}
+      <div className="mb-8">
+        <h3 className="text-xl font-semibold mb-6 flex items-center gap-3">
+          <FaBriefcase size={24} className="text-[var(--accent)]" />
+          Experience
+        </h3>
+        <ul className="relative z-0 border-l-3 border-[var(--accent)] pl-5 ml-4 space-y-6">
+          {/* Internship */}
+          <li className="relative">
+            <div className="absolute -left-7 top-3 w-3 h-3 bg-[var(--accent)] rounded-full border-2 border-white"></div>
+            <div className='flex items-start gap-4'>
+              <Image
+                src="/logo/pelco.png"
+                alt="Pampanga Electric Cooperative II IT Department"
+                width={50}  
+                height={50}
+                className="inline-block rounded-full"
+              />
+              <div>
+                <p className="font-semibold">IT Intern</p>
+                <p className="text-sm text-gray-600">
+                  Pampanga Electric Cooperative I – IT Department (February 2025 – May 2025)
+                </p>
+              </div>  
+            </div>
+          </li>
+        </ul>
+      </div>
 
       {/* Education */}
       <div className="mb-8">
